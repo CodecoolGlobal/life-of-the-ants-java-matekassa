50775d3399970c6027335c18c2aca53124a4fb33
Mate Kassa <mate.kassa@gmail.com>
2022-10-15 15:36:53 +0200

    Fix drone kickaway method

--- src/main/java/com/codecool/ants/Drone.java
+++ src/main/java/com/codecool/ants/Drone.java
@@ -22,29 +22,27 @@ public class Drone extends Ant {
         return arrayList;
     }
     public static void move(ArrayList<Square> droneSquares, Square[][] area) {
-        int[] queenPosition = Colony.getQueenPosition(area);
+        Square queenPosition = Colony.getQueenPosition(area);
         for (Square square: droneSquares) {
             if (isOnQueenPosition(queenPosition, square)) {
-                square.removeSquareStatus(SquareStatus.DRONE);
                 Drone.kickAway(square, area);
-                break;
             }
             for (int i = 0; i < area.length; i++) {
-                if (queenPosition[0] > square.getX() && Colony.isValidMoveSouth(square, moveLength)) {
+                if (queenPosition.getX() > square.getX() && Colony.isValidMoveSouth(square, moveLength) && !isOnQueenPosition(queenPosition, square)) {
                     Drone.moveSouth(square, area);
-                } else if (queenPosition[0] < square.getX() && Colony.isValidMoveNorth(square, moveLength)) {
+                } else if (queenPosition.getX() < square.getX() && Colony.isValidMoveNorth(square, moveLength) && !isOnQueenPosition(queenPosition, square)) {
                     Drone.moveNorth(square, area);
-                } else if (queenPosition[1] > square.getY() && Colony.isValidMoveEast(square, moveLength)) {
+                } else if (queenPosition.getY() > square.getY() && Colony.isValidMoveEast(square, moveLength) && !isOnQueenPosition(queenPosition, square)) {
                     Drone.moveEast(square, area);
-                } else if (queenPosition[1] < square.getY() && Colony.isValidMoveWest(square, moveLength)) {
+                } else if (queenPosition.getY() < square.getY() && Colony.isValidMoveWest(square, moveLength) && !isOnQueenPosition(queenPosition, square)) {
                     Drone.moveWest(square, area);
                 }
             }
         }
     }
 
-    public static boolean isOnQueenPosition(int[] queenPosition, Square square) {
-        return queenPosition[0] == square.getX() && square.getY() == queenPosition[1];
+    public static boolean isOnQueenPosition(Square queenPosition, Square square) {
+        return queenPosition.equals(square);
     }
 
     public static void moveEast(Square square, Square[][] area) {
@@ -64,18 +62,19 @@ public class Drone extends Ant {
         area[square.getX()+moveLength][square.getY()].addSquareStatus(SquareStatus.DRONE);
     }
 
-    public static void kickAway(Square square, Square[][] area) { // TODO
+    public static void kickAway(Square square, Square[][] area) {
+        square.removeSquareStatus(SquareStatus.DRONE);
         Random random = new Random();
         int moveLength = random.nextInt(area.length/2);
         Direction direction = Direction.randomDirection();
         if (direction.equals(Direction.EAST) && Colony.isValidMoveEast(square, moveLength)) {
-            Drone.moveEast(square, area);
+            area[square.getX()][square.getY()+moveLength].addSquareStatus(SquareStatus.DRONE);
         } else if (direction.equals(Direction.WEST) && Colony.isValidMoveWest(square, moveLength)) {
-            Drone.moveWest(square, area);
+            area[square.getX()][square.getY()-moveLength].addSquareStatus(SquareStatus.DRONE);
         } else if (direction.equals(Direction.NORTH) && Colony.isValidMoveNorth(square, moveLength)) {
-            Drone.moveNorth(square, area);
+            area[square.getX()-moveLength][square.getY()].addSquareStatus(SquareStatus.DRONE);
         } else if (direction.equals(Direction.SOUTH) && Colony.isValidMoveSouth(square, moveLength)) {
-            Drone.moveSouth(square, area);
+            area[square.getX()+moveLength][square.getY()].addSquareStatus(SquareStatus.DRONE);
         }
     }
 }
